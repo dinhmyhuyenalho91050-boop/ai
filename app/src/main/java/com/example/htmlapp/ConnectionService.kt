5af46e7b2541f52e77b864000fe49f5a07a56aa2
dinhmyhuyenalho91050-boop <dinhmyhuyenalho91050@gmail.com>
2025-10-19 21:00:33 +0800

    Add missing PackageManager import

--- app/src/main/java/com/example/htmlapp/ConnectionService.kt
+++ app/src/main/java/com/example/htmlapp/ConnectionService.kt
@@ -8,6 +8,7 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
