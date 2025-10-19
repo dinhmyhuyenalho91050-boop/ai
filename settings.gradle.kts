8d921ca2a5d161eff9330a7a609736feaa244977
dinhmyhuyenalho91050-boop <dinhmyhuyenalho91050@gmail.com>
2025-10-19 15:00:03 +0800

    feat: add android webview shell for index

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.3.2"
+        kotlin("android") version "1.9.23"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "immersive-index"
+include(":app")
