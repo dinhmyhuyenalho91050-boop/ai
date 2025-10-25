0b379a31639beccd3c3b3468cedafa7f9f93a9e7
dinhmyhuyenalho91050-boop <dinhmyhuyenalho91050@gmail.com>
2025-10-26 03:19:44 +0800

    fix: align kotlin version with compose compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "8.3.2" apply false
-    kotlin("android") version "1.9.23" apply false
-    kotlin("plugin.serialization") version "1.9.23" apply false
+    kotlin("android") version "1.9.24" apply false
+    kotlin("plugin.serialization") version "1.9.24" apply false
 }
